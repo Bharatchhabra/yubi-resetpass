5f15602cce0c563a10060a4c8784a177d32699f9
brinavneet <navneet.brinfotech@gmail.com>
2022-02-01 19:20:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import { ResetPasswordPage } from "./pages/loginpage/ResetPasswordPage";
+
+function App() {
+  return (
+    <div className="App">
+      <ResetPasswordPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/loginpage/ResetPasswordPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/layout/header/Header'
+import { ResetPassword } from './resetpassword/ResetPassword'
+
+export const ResetPasswordPage = () => {
+    return (
+        <div>
+            <Header />           
+            <ResetPassword />
+        </div>
+    )
+}
